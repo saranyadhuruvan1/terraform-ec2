2bd3441041f98ddba2b1efc598972aa3358a54c9
saranya <saranyadhuruvan2021@gmail.com>
2026-04-08 18:40:21 -0400

    Initial Terraform EC2 project

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "key_name" {}
